6fc2c2f014d2f3ee1d47169b0c37691843b0d1e5
AnastasiyaKl <anastasiakovalenko886@gmail.com>
2019-06-20 18:46:26 +0300

    new remote repository.

--- index.js
+++ index.js
@@ -15,14 +15,16 @@ const firebaseConfig = {
     appId: "1:419208145579:web:b38df2277851fe91"
 };
 
-const bot = new Telegraf(token, {
-    webHook: {
-        port: 3000
-    }
-});
-firebase.initializeApp(firebaseConfig);
+const bot = new Telegraf(token);
 
-const database = firebase.database();
+// const bot = new Telegraf(token, {
+//     webHook: {
+//         port: 3000
+//     }
+// });
+// firebase.initializeApp(firebaseConfig);
+//
+// const database = firebase.database();
 
 // firebase.database().ref('doc').set({
 //     field1: '1',
@@ -30,10 +32,10 @@ const database = firebase.database();
 // });
 
 // Set telegram webhook
-bot.telegram.setWebhook(`https://api.telegram.org/bot899478487:AAFK0VtsD7tOrbzp1UYso3ahiNy3Z090Msk/setWebhook?`);
+// bot.telegram.setWebhook(`https://api.telegram.org/bot899478487:AAFK0VtsD7tOrbzp1UYso3ahiNy3Z090Msk/setWebhook`);
 
 // Start https webhook
-bot.startWebhook('/setWebhook');
+// bot.startWebhook('/setWebhook');
 
 bot.hears('hi', ctx => {
     return ctx.reply('Hey!');
